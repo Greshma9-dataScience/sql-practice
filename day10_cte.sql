405ed855becb5770139b238138bb7118f82cb87e
Greshma_dev <ayenurugreshma@gmail.com>
2026-06-18 22:24:37 +0530

    Create day10_cte.sql

---
+++ day10_cte.sql
@@ -0,0 +1,31 @@
+
+
+-- Day 10: CTE (Common Table Expression)
+
+WITH HighScorers AS (
+    SELECT id, name, marks
+    FROM students
+    WHERE marks > 80
+)
+
+SELECT *
+FROM HighScorers;
+
+--instead of 
+
+SELECT *
+FROM (
+    SELECT id, name, marks
+    FROM students
+    WHERE marks > 80
+) AS HighScorers;
+
+WITH GradeAverage AS (
+    SELECT grade, AVG(marks) AS avg_marks
+    FROM students
+    GROUP BY grade
+)
+
+SELECT *
+FROM GradeAverage
+WHERE avg_marks > 75;
